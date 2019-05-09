8e6f84a0f831d59303289a616de6a72a59c38aa4
galaxysha512 <galaxysha512@gmail.com>
2019-05-09 15:57:27 +0800

    Successful

--- BaseExamples/src/main/java/com/sendtomoon/mybatis/baseExamples/BaseDAO.java
+++ BaseExamples/src/main/java/com/sendtomoon/mybatis/baseExamples/BaseDAO.java
@@ -1,5 +1,5 @@
 package com.sendtomoon.mybatis.baseExamples;
 
 public interface BaseDAO {
-
+	void add(Table1DTO dto);
 }
--- BaseExamples/src/main/java/com/sendtomoon/mybatis/baseExamples/Ex1.java
+++ BaseExamples/src/main/java/com/sendtomoon/mybatis/baseExamples/Ex1.java
@@ -2,8 +2,10 @@ package com.sendtomoon.mybatis.baseExamples;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -25,8 +27,13 @@ public class Ex1 {
 		try {
 			InputStream inputStream = Resources.getResourceAsStream(resource);
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			System.err.println(sqlSessionFactory.toString());
-
+			SqlSession sqlSession = sqlSessionFactory.openSession(true);
+			BaseDAO dao = sqlSession.getMapper(BaseDAO.class);
+			Table1DTO dto = new Table1DTO();
+			dto.setId(UUID.randomUUID().toString());
+			dto.setName("Test11111");
+			dto.setPrice(6543.21);
+			dao.add(dto);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
